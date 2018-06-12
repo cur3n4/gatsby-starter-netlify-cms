c69616c11a52b06bd079fd2f722be0354af8f1c0
Sergio Alvarez Diaz <sergio@sergiomac.local>
2018-06-12 11:01:44 +1000

    Trying to add genres

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,10 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
+
+        <Link className="navbar-item" to="/products">
+          Genres
+        </Link>
         <Link className="navbar-item" to="/products">
           Products
         </Link>
